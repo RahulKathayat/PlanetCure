8d639f18bdf8c8ffeceea07a925941d3deec3c2b
RahulKathayat <rkathayat1103@gmail.com>
2024-05-09 00:00:57 +0530

    globe added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { BackgroundBeams } from "~/components/ui/background-beams";
+import { GlobeDemo } from "./_components/GlobeDemo";
 
 export default function HomePage() {
   return (
     <main >
       <div>
         <BackgroundBeams className="sticky h-screen"/>
+      </div>
+      <div>
+        <GlobeDemo />
       </div>
     </main>
   );
