cb716da11405de6058add92597e189e180c1e1c0
RahulKathayat <rkathayat1103@gmail.com>
2024-05-08 17:25:30 +0530

    git commit fixed hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import "~/styles/globals.css";
 import "@uploadthing/react/styles.css";
-
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { ourFileRouter } from "~/app/api/uploadthing/core";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs';
 import TopNav from "./_components/TopNav";
+import { extractRouterConfig } from "uploadthing/server";
 
 
 
@@ -27,6 +29,15 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`font-sans ${inter.variable} dark`}>
+        <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
         <div className="h-screen grid grid-rows-[auto,1fr]">
           <TopNav/>
           <main className=" overflow-y-auto">
