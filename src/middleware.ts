1d02d2bd5777ce45dfe9f73592d9f6019d9bb104
RahulKathayat <rkathayat1103@gmail.com>
2024-05-09 23:08:55 +0530

    fixed few bugs

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import {
     createRouteMatcher
   } from "@clerk/nextjs/server" 
   
-  const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"])
+  const isProtectedRoute = createRouteMatcher(["/dashboard(.*)","/gemini-ai(.*)"])
   
   export default clerkMiddleware((auth, request) => {
     if (isProtectedRoute(request)) auth().protect()
